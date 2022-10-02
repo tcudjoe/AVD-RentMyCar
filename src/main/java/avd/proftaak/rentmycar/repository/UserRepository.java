2d5c211d9c89267ff53e3e42b63379f995216ab0
Tyra Cudjoe <tyra20015@gmail.com>
2022-10-02 16:19:11 +0200

    added user repository

---
+++ src/main/java/avd/proftaak/rentmycar/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package avd.proftaak.rentmycar.repository;
+
+import avd.proftaak.rentmycar.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+    private List<User> users = new ArrayList<>();
+
+
+}
