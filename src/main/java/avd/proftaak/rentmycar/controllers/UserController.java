54b40e8dfade03fef090830fc6fd3ea8ac8b346d
Tyra Cudjoe <tyra20015@gmail.com>
2022-10-24 18:16:43 +0200

    Quick commit

--- src/main/java/avd/proftaak/rentmycar/controllers/UserController.java
+++ src/main/java/avd/proftaak/rentmycar/controllers/UserController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 //Alles werkt hier ook. Ik heb hier ook delete mapping nog niet uitgeprobeerd maar dat doe ik ook wel
 @Slf4j
 @RestController
-@RequestMapping("/Users")
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
