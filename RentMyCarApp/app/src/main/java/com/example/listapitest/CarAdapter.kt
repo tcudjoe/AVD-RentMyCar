99ea18193372f6b64328c0a899b6dff26e785e9b
Jan-Paul <jan-paul@weareblis.com>
2022-12-20 15:36:26 +0100

    split projects

---
+++ RentMyCarApp/app/src/main/java/com/example/listapitest/CarAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.listapitest
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class CarAdapter(val context: Context) :  RecyclerView.Adapter<CarAdapter.CarViewHolder>() {
+    val client by lazy { RentMyCarApiClient.create() }
+    var cars: ArrayList<Car> = ArrayList()
+
+    init { refreshCars() }
+
+    class CarViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): CarAdapter.CarViewHolder {
+
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+
+        return CarViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CarViewHolder, position: Int) {
+        holder.view.model.text = cars[position].model
+        holder.view.brand.text = cars[position].brand
+
+    }
+
+    override fun getItemCount() = cars.size
+
+    fun refreshCars() {
+        client.getCars()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ result ->
+                cars.clear()
+                cars.addAll(result)
+                notifyDataSetChanged()
+            },{ error ->
+                Toast.makeText(context, "Refresh error: ${error.message}", Toast.LENGTH_LONG).show()
+                Log.e("ERRORS", error.message.toString())
+            })
+    }
+}
---
+++ RentMyCarApp/app/src/main/java/com/example/listapitest/RentMyCarApiClient.kt
@@ -0,0 +1,27 @@
+import com.example.listapitest.Car
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+
+interface RentMyCarApiClient {
+    @GET("cars") fun getCars(): Observable<ArrayList<Car>>
+
+    companion object {
+
+        fun create(): RentMyCarApiClient {
+
+            val retrofit = Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("http://10.0.2.2:8080/")
+                //.baseUrl("http://192.168.137.1:8080/")
+                //.baseUrl("http://10.180.124.215:8080/")
+
+                .build()
+
+            return retrofit.create(RentMyCarApiClient::class.java)
+        }
+    }
+}
