ff96a95c92c61a331862011c7b8ae95f1082f675
Tyra Cudjoe <tyra20015@gmail.com>
2023-01-13 14:09:20 +0100

    Feature: map is visible

--- RentMyCarApp/app/src/main/java/com/example/listapitest/MapsFragment.kt
+++ RentMyCarApp/app/src/main/java/com/example/listapitest/MapsFragment.kt
@@ -6,15 +6,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 class MapsFragment : Fragment() {
+    private lateinit var mMap: GoogleMap
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -44,4 +45,117 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+    fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+        // Avans Hogeschoollaan: 51.5840579881456, 4.797372671775713
+        val avansHA = LatLng(51.5840579881456, 4.797372671775713)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(avansHA))
+        // camera update with zoom:
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(avansHA, 8f))
+
+        val markerHA = mMap.addMarker(MarkerOptions().position(avansHA).title("Avans HA"))
+        markerHA?.tag = 0
+
+        mMap.setOnMarkerClickListener { marker ->
+            var clickCount = 1 + marker.tag as Int
+
+            marker.tag = clickCount
+
+//            Toast.makeText(this, "${marker.title} is $clickCount aangeklikt", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+
+        mMap.uiSettings.apply {
+            isZoomControlsEnabled = true
+            isTiltGesturesEnabled = true
+        }
+
+        mMap.apply {
+            setMinZoomPreference(8f)
+            setMaxZoomPreference(20f)
+        }
+
+//        mMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        val cameraPosition = CameraPosition.builder()
+            .target(avansHA)
+            .zoom(17f)
+            .bearing(90f)
+            .tilt(45f)
+            .build()
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+
+        // Add polylines to the map.
+        // Polylines are useful to show a route or some other connection between points.
+        val rondjeLD = googleMap.addPolyline(
+            PolylineOptions()
+                .clickable(true)
+                .add(
+                    LatLng(51.58589756724664, 4.794358257204294),
+                    LatLng(51.58621754669707, 4.795420411974192),
+                    LatLng(51.58612421959015, 4.796343091875315),
+                    LatLng(51.58590423350819, 4.796246532350779),
+                    LatLng(51.58562424968158, 4.7959246672689915),
+                    LatLng(51.58537092949463, 4.7952487505972385),
+                    LatLng(51.585444259167666, 4.794594291597605),
+                    LatLng(51.58563091598323, 4.794315341860056),
+                    LatLng(51.58589756724664, 4.794358257204294)
+                )
+        )
+
+        rondjeLD.tag = "rondje LD"
+
+        mMap.setOnPolylineClickListener { polyline ->
+            if (polyline.pattern == null) {
+                polyline.pattern = listOf<PatternItem>(Gap(20f), Dot())
+            } else {
+                polyline.pattern = null
+            }
+        }
+
+
+//        addMarkers(mMap)
+//        addClusterdMarkers()
+//
+        // camera position depending on Clustered markers:
+        // setOnMapLoadedCallback: Sets a callback that's invoked when this map has finished rendering.
+//        mMap.setOnMapLoadedCallback {
+//            val bounds = LatLngBounds.builder()
+//            blindwalls.forEach { blindWall ->
+//                bounds.include(blindWall.latLng)
+//            }
+//            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 50))
+//        }
+//
+//        mMap.setInfoWindowAdapter(BlindWallMarkerAdapter(this))
+
+
+    }
+
+    // wordt na het maken van clustered items miet meer benut.
+//    private fun addMarkers() {
+//        blindwalls.forEach { blindWall ->
+//            val marker = mMap.addMarker(
+//                MarkerOptions()
+//                    .title(blindWall.name)
+//                    .position(blindWall.latLng)
+//            )
+//            marker?.tag = blindWall
+//        }
+//    }
+//
+//    private fun addClusterdMarkers() {
+//        val clusterManager = ClusterManager<>(this, mMap)
+//        clusterManager.addItems(blindwalls)
+//        clusterManager.cluster()
+//
+//        mMap.setOnCameraIdleListener {
+//            clusterManager.onCameraIdle()
+//        }
+//    }
+
 }
