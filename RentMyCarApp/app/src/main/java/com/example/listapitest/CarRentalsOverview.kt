5a8a6badcb63c68e877f655bd789c7c31719e27f
Jan-Paul <jan-paul@weareblis.com>
2023-01-13 15:47:26 +0100

    add shared preferences

--- RentMyCarApp/app/src/main/java/com/example/listapitest/CarRentalsOverview.kt
+++ RentMyCarApp/app/src/main/java/com/example/listapitest/CarRentalsOverview.kt
@@ -1,25 +1,33 @@
 package com.example.listapitest
 
+import android.R.attr.password
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.listapitest.databinding.CarRentalsOverviewBinding
 import kotlinx.android.synthetic.main.car_rentals_overview.*
 
+
 class CarRentalsOverview : Fragment() {
 
     private var _binding: CarRentalsOverviewBinding? = null
-    lateinit var adapter:CarAdapter
+    private lateinit var adapter:CarAdapter
     private var baseContext:Context? = null
     private val binding get() = _binding!!
+    private val sharedPrefFile = "kotlinsharedpreference"
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var edit: SharedPreferences.Editor
+
+    private lateinit var maxKilometers: String;
+    private lateinit var maxCost: String;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +40,14 @@ class CarRentalsOverview : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         baseContext = view.context
         adapter = CarAdapter(view.context)
+        sharedPreferences = view.context.getSharedPreferences(sharedPrefFile,Context.MODE_PRIVATE)
+        edit = sharedPreferences.edit()
+
+        maxKilometers = sharedPreferences.getString("max_kilometers_input", "") ?: "";
+        maxCost = sharedPreferences.getString("max_cost_input", "") ?: "";
+
+        max_kilometers_input.setText(maxKilometers);
+        max_cost_input.setText(maxCost);
 
         car_item_list.layoutManager = LinearLayoutManager(view.context)
         car_item_list.adapter = adapter
@@ -63,6 +79,13 @@ class CarRentalsOverview : Fragment() {
 
     private fun getCars()
     {
-        adapter.getCars(max_kilometers_input.text.toString(), max_cost_input.text.toString())
+        val max_kilometers_input = max_kilometers_input.text.toString();
+        val max_cost_input = max_cost_input.text.toString();
+
+        edit.putString("max_kilometers_input", max_kilometers_input)
+        edit.putString("max_cost_input", max_cost_input)
+        edit.apply()
+
+        adapter.getCars(max_kilometers_input, max_cost_input)
     }
 }
