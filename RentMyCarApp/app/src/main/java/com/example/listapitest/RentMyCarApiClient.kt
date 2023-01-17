1d6d0986d85993a913c1775052308f0e972e82b3
Jan-Paul <jan-paul@weareblis.com>
2023-01-17 13:40:56 +0100

    fix port

--- RentMyCarApp/app/src/main/java/com/example/listapitest/RentMyCarApiClient.kt
+++ RentMyCarApp/app/src/main/java/com/example/listapitest/RentMyCarApiClient.kt
@@ -8,7 +8,6 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
 
-
 interface RentMyCarApiClient {
     @GET("cars")
     fun getCars(@Query("maxKilometers") kilometers:String, @Query("maxCost") cost:String): Observable<ArrayList<Car>>
@@ -22,7 +21,7 @@ interface RentMyCarApiClient {
             val retrofit = Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
-                .baseUrl("http://10.0.2.2:8081/")
+                .baseUrl("http://10.0.2.2:8080/")
                 .build()
 
             return retrofit.create(RentMyCarApiClient::class.java)
