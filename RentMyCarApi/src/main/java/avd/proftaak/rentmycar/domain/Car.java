bdc87cb313cc943e120afb7d15fdb806a8c48eae
Tyra Cudjoe <tyra20015@gmail.com>
2023-01-22 17:29:11 +0100

    Singleton pattern

--- RentMyCarApi/src/main/java/avd/proftaak/rentmycar/domain/Car.java
+++ RentMyCarApi/src/main/java/avd/proftaak/rentmycar/domain/Car.java
@@ -22,6 +22,7 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "carId", nullable = false)
     private Long carId;
+    //holds one instance of clas Car, makes Car accessible from multiple threads and prevents caching problems
     private static volatile Car car;
     private String brand;
     private String model;
@@ -38,6 +39,7 @@ public class Car {
     //@JoinColumn(name = "rentalserviceId", nullable = false)
     private RentalService rentalService;
 
+    //Private constructor, only Car can instantiate this class
     private Car(String brand, String model, int yearOfBuild, Integer kilometers, double weight, CarCategories category, String location, double cost) {
         this.brand = brand;
         this.model = model;
@@ -51,6 +53,7 @@ public class Car {
 
     public static Car getInstance(String brand, String model, int yearOfBuild, Integer kilometers, Double weight, CarCategories category, String location, double cost){
         if (car == null){
+            //We get exclusive access to the code block using synchronized
             synchronized (Car.class){
                 if (car == null){
                     System.out.println("Creating an object: ");
